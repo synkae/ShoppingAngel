3e284616fe7d7dc703bb1716e400961bcf903267
MoniBarrios <49417323+MoniBarrios@users.noreply.github.com>
2021-04-15 12:13:46 -0700

    Main Activity Budget

---
+++ app/src/main/java/com/codepath/synkae/shoppingangel/UserMainActivity.java
@@ -0,0 +1,25 @@
+package com.codepath.synkae.shoppingangel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class UserMainActivity extends AppCompatActivity {
+
+    private EditText adjustBudget;
+    private Button adjustBtn;
+    private Button logoutBtn;
+    private TextView currBudget;
+    private TextView remainingBudget;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_main);
+
+
+    }
+}
